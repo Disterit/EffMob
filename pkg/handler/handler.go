fceebfe3bbb2c21298ab1de955fd38145b6696d0
Dister <Levchikov.zhenya@mail.ru>
2024-11-28 18:10:02 +0700

    struct handler

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -16,5 +16,20 @@ func NewHandler(service *service.Service) *Handler {
 func (h *Handler) InitRoutes() *gin.Engine {
 	router := gin.New()
 
+	song := router.Group("/song")
+	{
+		song.POST("")       // добавление песни
+		song.GET("")        // получение всех песен
+		song.GET("/:id")    // получение песни по id
+		song.PUT("/:id")    // обновление песни по id
+		song.DELETE("/:id") // удаление песни по id
+
+		verse := song.Group("/:id/verse/:verse")
+		{
+			verse.GET("") // получение 1 или более куплетов с параметром limit который указывает на колличество куплетов которое будет получено после указанного
+			verse.PUT("") //обновление куплета по id
+		}
+	}
+
 	return router
 }
