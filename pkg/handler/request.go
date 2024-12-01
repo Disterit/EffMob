1b9a22823b6d1626c9b3ccf4f382b3af6170221e
Dister <Levchikov.zhenya@mail.ru>
2024-12-01 21:35:32 +0700

    return request

--- pkg/handler/request.go
+++ pkg/handler/request.go
@@ -2,7 +2,11 @@ package handler
 
 import (
 	"EffMob/models"
+	"encoding/json"
 	"errors"
+	"fmt"
+	"net/http"
+	"net/url"
 )
 
 var (
@@ -10,39 +14,28 @@ var (
 	ErrNoResponse = errors.New("no response")
 )
 
-//func GetSongInfo(externalApiUrl, group, song string) (*models.SongInfo, error) {
-//	encodedGroup := url.QueryEscape(group)
-//	encodedSong := url.QueryEscape(song)
-//
-//	resp, err := http.Get(fmt.Sprintf("%s/info?group=%s&song=%s", externalApiUrl, encodedGroup, encodedSong))
-//	if err != nil {
-//		return nil, err
-//	}
-//	defer resp.Body.Close()
-//
-//	if resp.StatusCode != http.StatusOK {
-//		if resp.StatusCode == http.StatusBadRequest {
-//			return nil, ErrBadRequest
-//		} else {
-//			return nil, ErrNoResponse
-//		}
-//	}
-//
-//	var songInfo models.SongInfo
-//	if err = json.NewDecoder(resp.Body).Decode(&songInfo); err != nil {
-//		return nil, err
-//	}
-//
-//	return &songInfo, nil
-//}
-
 func GetSongInfo(externalApiUrl, group, song string) (*models.SongInfo, error) {
+	encodedGroup := url.QueryEscape(group)
+	encodedSong := url.QueryEscape(song)
+
+	resp, err := http.Get(fmt.Sprintf("%s/info?group=%s&song=%s", externalApiUrl, encodedGroup, encodedSong))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		if resp.StatusCode == http.StatusBadRequest {
+			return nil, ErrBadRequest
+		} else {
+			return nil, ErrNoResponse
+		}
+	}
 
 	var songInfo models.SongInfo
-
-	songInfo.Text = "Ooh baby, don't you know I suffer?\nOoh baby, can you hear me moan?\nYou caught me under false pretenses\nHow long before you let me go?\n\nOoh\nYou set my soul alight\nOoh\nYou set my soul alight"
-	songInfo.Link = "https://www.youtube.com/watch?v=Xsp3_a-PMTw"
-	songInfo.ReleaseDate = "16.07.2006"
+	if err = json.NewDecoder(resp.Body).Decode(&songInfo); err != nil {
+		return nil, err
+	}
 
 	return &songInfo, nil
 }
